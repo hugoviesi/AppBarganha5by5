3c36d932f0ca783fa7a2eddde6afed5b3835e569
hugoviesi <viesihugo@hotmail.com>
2021-04-19 20:26:39 -0300

    categoria

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -2,10 +2,38 @@
 {
     public enum Categoria
     {
-        ROUPAS = 1,
-        MOVEIS = 2,
-        IMOVEIS = 3,
-        ELETRONICOS = 4,
-        AUTOMOVEIS = 5
+        ACESSORIOSVEICULOS = 1,
+        AUTOMOVEIS = 2,
+        AGRO = 3,
+        ALIMENTOS = 4,
+        ANIMAIS = 5,
+        ANTIGUIDADES = 6,
+        BEBIDAS = 7,
+        BIJUTERIA = 8,
+        BEBES = 9,
+        BRINQUEDOS = 10,
+        CELULARES = 11,
+        CONSTRUCAO = 12,
+        COMERCIO = 13,
+        CALCADOS = 14,
+        DECORACAO = 15,
+        ELETRODOMESTICOS = 16,
+        ELETRONICOS = 17,
+        ESPORTE = 18,
+        ESTETICA = 19,
+        FERRAMENTAS = 20,
+        FESTAS = 21,
+        FOTOGRAFIA = 22,
+        GAMES = 23,
+        GEEK = 24,
+        IMOVEIS = 25,
+        INDUSTRIA = 26,
+        INFORMATICA = 27,
+        MUSICA = 28,
+        PAPELARIA = 29,
+        ROUPAS = 30,
+        SAUDE = 31,
+        SERVICOS = 32,
+        OUTROS = 33
     }
 }
